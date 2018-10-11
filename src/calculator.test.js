dbf5eb957191a5471f2ca27b9485130bcc38d7b3
arnabjorg17 <34029748+arnabjorg17@users.noreply.github.com>
2018-10-11 15:31:57 +0000

    Implemented for one number

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -2,5 +2,10 @@ const add = require("./calculator");
 
 it("should return zero on an empty string", () => 
 {
-  expect(add(" ")).toBe(0);
+  expect(add("")).toBe(0);
+});
+
+it("should return number when there is only one numbe in the string", () =>
+{
+  expect(add("4")).toBe(4);
 });
